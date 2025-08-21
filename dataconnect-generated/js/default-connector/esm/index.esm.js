eda39d007c8eeb258911a197aa5f71d9e76ac84b
panchlml7 <panchoramos39@gmail.com>
2025-08-21 08:49:42 -0400

    feat(docs): Documentos compartidos en Firestore global; reglas actualizadas; UI: edición para todos y borrado solo dueño. Planilla: limpieza. Firebase config y reglas añadidas.

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'plataform-slepiqq-main',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'plataform-slepiqq-main',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
